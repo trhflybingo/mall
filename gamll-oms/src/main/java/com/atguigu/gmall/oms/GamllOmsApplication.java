18b469d585ef9730b5a19aedf6a300c207b83050
e <623490406@qq.com>
2019-12-20 16:47:37 +0800

    项目结构创建完成

---
+++ gamll-oms/src/main/java/com/atguigu/gmall/oms/GamllOmsApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.oms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamllOmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamllOmsApplication.class, args);
+    }
+
+}
